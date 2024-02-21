025d6b2b7b3f59c6864865811a4d9a6fa16e98d9
Kami <ludovic.chevroulet@outlook.fr>
2024-02-21 18:43:42 +0100

    dev: back initialisation done

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,9 +1,15 @@
+// api/routes/index.ts
 import express from 'express';
 import productsRouter from './productsRouter'; // Import en tant qu'export par défaut
-console.log('coucou')
+
 const router = express.Router();
-console.log('Router: ', router);
+
 // Utilisez pnjRouter pour gérer toutes les routes commençant par '/pnj'
 router.use('/products', productsRouter);
+
+// Montez la route `/test` sur l'application Express principale
+router.get('/test', async (req, res) => {
+  res.send('Hello from /test');
+});
 
 export default router;
--- api/routes/productsRouter.ts
+++ api/routes/productsRouter.ts
@@ -1,27 +1,21 @@
+// api/productsRouter.ts
 import express from 'express';
 import { getConnection } from '../config/db'; // Import corrigé
-import { log } from 'console';
-
-interface RowData {
-  firstname: string;
-}
 
 const router = express.Router();
 
-router.get('/products', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    console.log('Products');
-    
     // Récupérez la connexion à la base de données
     const connection = getConnection(); // Utilisation de getConnection()
 
-    // Exécutez votre requête SQL pour récupérer un prénom aléatoire
-    const [rows]: any[] = await connection.query('SELECT * FROM `products` WHERE 1 ORDER BY RAND() LIMIT 1');
+    // Exécutez votre requête SQL pour récupérer tous les produits
+    const [rows]: any[] = await connection.query('SELECT * FROM `products` WHERE 1');
 
-    // Envoyez le prénom aléatoire en réponse
-    res.json({ firstname: rows[0].label });
+    // Envoyez les produits en réponse
+    res.json(rows);
   } catch (error) {
-    console.error('Erreur lors de la récupération du prénom : ', error);
+    console.error('Erreur lors de la récupération des produits : ', error);
     res.status(500).send('Erreur interne du serveur');
   }
 });
